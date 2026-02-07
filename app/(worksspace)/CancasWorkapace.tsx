2bea609d3b88feacf92fa9a2736e154df5543e33
steven-zhang <yustevenzhang@gmail.com>
2026-02-07 12:53:46 +0800

    Enhance CancasWorkapace component layout by adding ToolBar and structured div elements for workspace, background, and sidebar.

--- app/(worksspace)/CancasWorkapace.tsx
+++ app/(worksspace)/CancasWorkapace.tsx
@@ -1,6 +1,18 @@
+import ToolBar from "./components/ToolBar"
 
 export default function CancasWorkapace() {
     return (
-        <div>CancasWorkapace</div>
+        <div id="cancas-workspace" className="h-full w-full">
+            <div id="world"
+                className="h-full w-full absolute top-0 left-0 z-2"
+            ></div>
+            <div id="background"
+                className="h-full w-full absolute top-0 left-0 z-1"
+            ></div>
+            <ToolBar />
+            <div id="sidebar"></div>
+            <div>CancasWorkapace</div>
+        </div>
+
     )
 }
---
+++ app/(worksspace)/components/ToolBar.tsx
@@ -0,0 +1,16 @@
+
+
+export default function ToolBar() {
+    return (
+        <div id="toolbar"
+            className="h-25 w-full absolute mb-10 flex items-center justify-center bottom-0"
+        >
+            <div id="toolbar-background"
+                className="h-full w-[80%] absolute  bg-amber-400"
+            >
+
+            </div>
+
+        </div>
+    )
+}
