3fd5305b81a10ccc6be3bd28b677fd4a705313d0
steven-zhang <yustevenzhang@gmail.com>
2026-02-08 23:20:17 +0800

    refactor: clean up Viewport component by removing unnecessary whitespace and improving readability

--- app/(worksspace)/components/Viewport.tsx
+++ app/(worksspace)/components/Viewport.tsx
@@ -4,11 +4,6 @@ import World from './World'
 import { Camera } from '../type/camera';
 import { useViewportControls } from '../hooks/useViewPortControls';
 
-
-
-
-
-
 export default function Viewport() {
     const [cam, setCam] = useState<Camera>({ tx: 0, ty: 0, scale: 1 });
     const { handlers } = useViewportControls(cam, setCam);
