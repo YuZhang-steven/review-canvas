a23b69dcd515794965696f1f908051609c109b75
steven-zhang <yustevenzhang@gmail.com>
2026-02-08 19:58:44 +0800

    feat: implement zustand store for managing current selected ID

---
+++ app/(worksspace)/state/useCurrentSeletctId.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+interface CurrentSelectedIdState {
+  currentSelectedId: string | null;
+  setCurrentSelectedId: (id: string | null) => void;
+  clearCurrentSelectedId: () => void;
+}
+
+export const useCurrentSelectedIdStore = create<CurrentSelectedIdState>((set) => ({
+  currentSelectedId: null,
+  setCurrentSelectedId: (id) => set({ currentSelectedId: id }),
+  clearCurrentSelectedId: () => set({ currentSelectedId: null }),
+}));
+
